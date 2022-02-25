adbad8f1a508b123213af480068dda6425dcdcaf
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-25 18:43:06 -0300

    Upadate 1

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+var Resultado = 0, Valor1 = 0, Valor2 = 0
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +11,46 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    color: 'cccccc',
+    fontSize: 25
+  },
+  input: {
+    margin: 5,
+    borderColor: '#c1c1c1',
+    borderWidth: 2,
+    borderRadius: 30,
+    fontSize: 18,
+    textAlign: 'center',
+    width: '100%'
+  },
+  bloco: {
+    margin: 5
+  },
+  btn: {
+    margin: 5,
+    borderColor: '#c1c1c1',
+    borderWidth: 2,
+    borderRadius: 30,
+    fontSize: 18,
+    textAlign: 'center',
+    width: '100%'
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Prototipo de alguma coisa</Text>
+      <StatusBar style="auto" />
+      <View style={styles.bloco}>
+        <Text>Valor 1:</Text>
+        <TextInput style={styles.input} placeholder={'Primeiro valor'} />
+        <Text>Valor 2:</Text>
+        <TextInput style={styles.input} placeholder={'Segundo valor'} />
+        <TouchableOpacity style={styles.btn}><Text>Somar</Text></TouchableOpacity>
+        <Text>Resultado: {Resultado}</Text>
+      </View>
+    </View>
+  );
+}
