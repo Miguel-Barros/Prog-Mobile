c2ea33ecd2bf98c4013efcd9781dece5def23074
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-03-04 18:33:22 -0300

    Adicionando função

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
+import { React, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button, TextInput, TouchableOpacity } from 'react-native';
-
-var Resultado = 0, Valor1 = 0, Valor2 = 0
+import { TextInput, TouchableOpacity } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -25,7 +24,7 @@ const styles = StyleSheet.create({
     width: '100%'
   },
   bloco: {
-    margin: 5
+    margin: 5,
   },
   btn: {
     margin: 5,
@@ -34,22 +33,37 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     fontSize: 18,
     textAlign: 'center',
-    width: '100%'
+    width: '100%',
+    display: 'flex'
   }
 });
 
 export default function App() {
+
+  const [valor1, setValor1] = useState();
+  const [valor2, setValor2] = useState();
+  const [result, setResult] = useState();
+
+  function somar(){
+    setResult(parseFloat(valor1) + parseFloat(valor2))
+  }
+
+  function reset(){
+    setResult(null);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Prototipo de alguma coisa</Text>
       <StatusBar style="auto" />
       <View style={styles.bloco}>
         <Text>Valor 1:</Text>
-        <TextInput style={styles.input} placeholder={'Primeiro valor'} />
+        <TextInput style={styles.input} value={valor1} keyboardType='numeric' placeholder={'Primeiro valor'} onChangeText={(texto)=>setValor1(texto) } />
         <Text>Valor 2:</Text>
-        <TextInput style={styles.input} placeholder={'Segundo valor'} />
-        <TouchableOpacity style={styles.btn}><Text>Somar</Text></TouchableOpacity>
-        <Text>Resultado: {Resultado}</Text>
+        <TextInput style={styles.input} value={valor2} keyboardType='numeric' placeholder={'Segundo valor'} onChangeText={(texto)=>setValor2(texto) } />
+        <TouchableOpacity style={styles.btn} onPress={somar}><Text>Calcular</Text></TouchableOpacity> 
+        <TouchableOpacity style={styles.btn} onPress={reset}><Text>Reiniciar</Text></TouchableOpacity>
+        <Text>Resultado: {result}</Text>
       </View>
     </View>
   );
